66113228a10f5cfdc581c9d70cf7fa72d492fe71
unknown <padil2246@gmail.com>
2024-10-26 18:43:19 +0530

    added load

--- src/components/NavbarPublic.tsx
+++ src/components/NavbarPublic.tsx
@@ -9,7 +9,7 @@ export default function NavbarPublic() {
 
   return (
     <nav className=" navbar fixed w-full px-10 py-5 z-50 flex items-center justify-between">
-      <Link href="/"><Image src={logo} alt="ProfileCraft" height={50} width={50} /></Link>
+      <Link href="/"><Image src={logo} alt="ProfileCraft" className="w-10 sm:w-12" /></Link>
       
 
         <ModeToggle />
