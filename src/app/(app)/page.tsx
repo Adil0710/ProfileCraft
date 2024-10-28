f32cde170ba2dcbc5f33e4c768411882fb6af778
Adil0710 <padil2246@gmail.com>
2024-10-28 17:07:29 +0530

    issues done

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,4 +1,4 @@
-import { ModeToggle } from "@/components/theme-toggle";
+
 import Image from "next/image";
 
 export default function Home() {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
-import NavbarPublic from "@/components/NavbarPublic";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
