aa185a841bfd9f360f6d8de9dc230b0cb3ecd97a
unknown <padil2246@gmail.com>
2024-10-31 22:56:50 +0530

    added margin

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -137,7 +137,7 @@ export default function LogIn() {
       <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 sm:px-0 px-2">
         <div className="flex items-center justify-center py-12">
           <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
+            <div className="grid gap-2 text-center sm:mt-0 mt-10">
               <h1 className="text-3xl font-bold">Login</h1>
               {/* <p className="text-balance text-muted-foreground">
                 Enter your email below to login to your account
