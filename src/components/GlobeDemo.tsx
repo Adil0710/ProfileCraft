64db29ac4b40a0e43a41833846a037e0c5bf1fd1
Adil0710 <padil2246@gmail.com>
2024-12-16 13:16:12 +0530

    changed location card

--- src/components/GlobeDemo.tsx
+++ src/components/GlobeDemo.tsx
@@ -395,7 +395,7 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center h-screen md:h-auto dark:bg-black bg-transparent relative w-full">
+    <div className="flex flex-row items-center justify-center h-screen md:h-auto bg-transparent relative w-full">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[28rem]">
         <motion.div
           initial={{
