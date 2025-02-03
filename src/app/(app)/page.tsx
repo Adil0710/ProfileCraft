2d818740c9c6e676317e9d07af54f5ad865c735a
Adil0710 <padil2246@gmail.com>
2025-02-03 12:30:54 +0530

    done

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,16 +2,13 @@ import { Features } from "@/components/Features";
 import Hero from "@/components/Hero";
 import Howitworks from "@/components/Howitworks";
 import Showcase from "@/components/Showcase";
-import BlurryBlob from "@/components/ui/blurry-blob";
 
 export default function Home() {
   return (
     <div className=" max-w-screen">
       <Hero />
       <Howitworks />
-
       <Features />
-
       <Showcase />
     </div>
   );
