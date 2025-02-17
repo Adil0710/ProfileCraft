0c0930898c9bb566ae8bd1d0b8677890f32e9aa4
Adil0710 <padil2246@gmail.com>
2025-02-17 12:09:36 +0530

    added footer

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Button } from "./ui/button";
+
 import { cn } from "@/lib/utils";
 import Header from "./ui/header";
 import { motion } from "framer-motion";
--- src/components/magicui/rainbow-button.tsx
+++ src/components/magicui/rainbow-button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 interface RainbowButtonProps
+ // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const RainbowButton = React.forwardRef<
